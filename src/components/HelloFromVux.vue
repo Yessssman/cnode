abd902322f56713db4f64746bf0345035e6af922
Yessssman <xiaozhao2233@gmail.com>
2018-06-29 14:09:35 +0800

    test

---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+   <h6>测试actionsheet</h6>
+    <actionsheet v-model="show" :menus="menus" show-cancel theme="android" @on-click-menu="myclick"></actionsheet>
+  </div>
+</template>
+
+<script>
+import { Group, Cell } from 'vux'
+import { Actionsheet } from 'vux'
+
+export default {
+  components: {
+    Actionsheet
+  },
+  data () {
+    return {
+      show: true,
+      menus: {
+        menu1: '分享到朋友圈',
+        menu2: '分享到新浪微博'
+      }
+    }
+  },
+  methods: {
+    myclick: function (menukey, menuitem) {
+      console.log(menukey)
+      console.log(menuitem)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
