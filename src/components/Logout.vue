93cfd08c39a496c83337623c31b923d227b0960d
Yessssman <xiaozhao2233@gmail.com>
2018-07-03 16:16:54 +0800

    login

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,10 +1,23 @@
 <template>
-
+  <div>
+    {{loginname}}您已注销，5秒后返回主页
+  </div>
 </template>
 
 <script>
     export default {
-        name: "Logout"
+        name: "Logout",
+      data() {
+        return {
+          loginname: ''
+        }
+      },
+      created() {
+          this.loginname = window.localStorage.getItem('loginname');
+          window.localStorage.clear();
+
+          setTimeout(this.$router.push('/#/topiclist/all'),)
+      }
     }
 </script>
 
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,6 +5,13 @@ const mutations = {
  mutationLogin: function (state) {
    state.isLogined = true;
    console.log(state.isLogined)
- }
+ },
+  mutationUserInfo: function (state, payload) {
+    state.userinfo = {
+      loginname: payload.userinfo.loginname,
+      id: payload.userinfo.id,
+      avatar_url: payload.userinfo.avatar_url
+    }
+  }
 }
 export default mutations
